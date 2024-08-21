cb068bb2c0dc22b939a5ab149667a4aaf6c4b5cf
Duvvuri Naga Pratyusha <39739586+pratyusha1213@users.noreply.github.com>
2024-08-21 20:54:10 +0300

    Added constants.js

---
+++ src/constants.js
@@ -0,0 +1,59 @@
+
+export const services = [
+    {
+        title: "Aspiring Python Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Entry-Level Data Scientist",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Oracle BPM Developer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Pratyusha';
+
+export const experiences = [
+    {
+        'company': 'Capgemini',
+        'role': 'SCUBI Support Maintenance',
+        'duration': 'OCTOBER 2023 – APRIL 2024',
+        'logo': 'https://1000logos.net/wp-content/uploads/2021/08/Capgemini-Logo-500x315.png',
+        'points': [
+            'Role: OSB Developer',
+            'Developed and maintained Oracle Service Bus (OSB) solutions to integrate various components of the SCUBI system, ensuring smooth data flow and communication.',
+            'Managed SQL databases, including performing regular maintenance, optimization, and complex data extraction for reporting and analysis.',
+            'Authored comprehensive technical documentation outlining system architecture, service configurations, and troubleshooting procedures to aid in future maintenance and development.',
+            'Created and executed test cases to validate functionality and performance of OSB services, ensuring high-quality and reliable code.',
+            'Conducted code reviews and refactoring to optimize performance and improve maintainability.',
+            'Collaborated with cross-functional teams, including business analysts and QA teams, to understand project requirements and deliver effective solutions.',
+            'Implemented security protocols to safeguard sensitive data and comply with regulatory standards.'
+        ],
+        'url': 'https://capgemini.com/',
+    },
+    {
+        'company': 'Capgemini',
+        'role': 'Atradius BBT Project',
+        'duration': 'FEBRUARY 2021 – JULY 2023',
+        'logo': 'https://1000logos.net/wp-content/uploads/2021/08/Capgemini-Logo-500x315.png',
+        'points': [
+            'Role: Oracle BPM/SOA Developer, Solution Analyst',
+            'Developed and implemented Oracle BPM/SOA solutions for a trade credit insurance company, streamlining business processes and enhancing operational efficiency.',
+            'Integrated various services from database (SQL) to middleware (SOA, OSB), ensuring seamless interaction and data consistency across systems.',
+            'Conducted unit testing and performance validation using tools like SoapUI and Postman, ensuring that the implemented solutions met functional and performance requirements.',
+            'Maintained project files in GitHub, ensuring proper version control and collaboration within the development team.',
+            'Prepared and managed project documentation, including system designs, process flows, and reporting, to provide clear insights and maintain project integrity.',
+            'Provided ongoing support and enhancements based on user feedback and changing business needs.'
+        ],
+        'url': 'https://capgemini.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
